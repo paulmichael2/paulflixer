2d5d9fac8810249eea6eeeba754b41308aec62e4
paulmichael2 <50063194+paulmichael2@users.noreply.github.com>
2023-06-06 20:10:37 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,6 @@
-export default {
-  //Site display name
-  SITE_TITLE: "Ripper+",
-
-  //Put your discord link here or null to disable
-  SITE_DISCORD: "https://ripper.lol/discord.html",
-  
-  //True or false to show or hide site credit in footer (You should leave enabled 😉)
-  SITE_CREDITS: true,
+ SITE_CREDITS: true,
 
   //Do not change or site will break!
   RIPPER_API: "https://api.ripper.fun"
+  RIPPER_API: "https://api.riptv.net"
 }
